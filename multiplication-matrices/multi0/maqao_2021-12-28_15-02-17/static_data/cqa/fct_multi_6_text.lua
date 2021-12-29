205a547b0a69c4ee783b319b96646685434cd98d
hamidaniarezkitakfa <arezkitakfa@gmail.com>
2021-12-29 14:07:01 +0100

    amelioration de performance de produit matrice*matrice avec MaQAO

---
+++ multiplication-matrices/multi0/maqao_2021-12-28_15-02-17/static_data/cqa/fct_multi_6_text.lua
@@ -0,0 +1,152 @@
+_cqa_text_report = {
+  paths = {
+    {
+      hint = {
+        {
+          title = "Type of elements and instruction set",
+          txt = "No instructions are processing arithmetic or math operations on FP elements. This function is probably writing/copying data or processing integer elements.",
+        },
+        {
+          title = "Matching between your function (in the source code) and the binary function",
+          txt = "The binary function does not contain any FP arithmetical operations.\nThe binary function is storing 20 bytes.",
+        },
+      },
+      expert = {
+        {
+          title = "General properties",
+          txt = "nb instructions    : 10\nnb uops            : 9\nloop length        : 31\nused x86 registers : 4\nused mmx registers : 0\nused xmm registers : 0\nused ymm registers : 0\nused zmm registers : 0\nnb stack references: 3\n",
+        },
+        {
+          title = "Front-end",
+          txt = "MACRO FUSION NOT POSSIBLE\nFIT IN UOP CACHE\nmicro-operation queue: 2.25 cycles\nfront end            : 2.25 cycles\n",
+        },
+        {
+          title = "Back-end",
+          txt = "       | P0   | P1   | P2   | P3   | P4   | P5   | P6   | P7\n--------------------------------------------------------------\nuops   | 0.50 | 0.25 | 1.67 | 1.67 | 4.00 | 0.25 | 2.00 | 1.67\ncycles | 0.50 | 0.25 | 1.67 | 1.67 | 4.00 | 0.25 | 2.00 | 1.67\n\nCycles executing div or sqrt instructions: NA\n",
+        },
+        {
+          title = "Cycles summary",
+          txt = "Front-end : 2.25\nDispatch  : 4.00\nOverall L1: 4.00\n",
+        },
+        {
+          title = "Vectorization ratios",
+          txt = "all     : 0%\nload    : NA (no load vectorizable/vectorized instructions)\nstore   : 0%\nmul     : NA (no mul vectorizable/vectorized instructions)\nadd-sub : NA (no add-sub vectorizable/vectorized instructions)\nfma     : NA (no fma vectorizable/vectorized instructions)\ndiv/sqrt: NA (no div/sqrt vectorizable/vectorized instructions)\nother   : NA (no other vectorizable/vectorized instructions)\n",
+        },
+        {
+          title = "Vector efficiency ratios",
+          txt = "all     : 20%\nload    : NA (no load vectorizable/vectorized instructions)\nstore   : 20%\nmul     : NA (no mul vectorizable/vectorized instructions)\nadd-sub : NA (no add-sub vectorizable/vectorized instructions)\nfma     : NA (no fma vectorizable/vectorized instructions)\ndiv/sqrt: NA (no div/sqrt vectorizable/vectorized instructions)\nother   : NA (no other vectorizable/vectorized instructions)\n",
+        },
+        {
+          title = "Cycles and memory resources usage",
+          txt = "Assuming all data fit into the L1 cache, each call to the function takes 4.00 cycles. At this rate:\n - 15% of peak store performance is reached (5.00 out of 32.00 bytes stored per cycle (GB/s @ 1GHz))\n",
+        },
+        {
+          title = "Front-end bottlenecks",
+          txt = "Found no such bottlenecks.",
+        },
+        {
+          title = "ASM code",
+          txt = "In the binary file, the address of the function is: 78a\n\nInstruction               | Nb FU | P0   | P1   | P2   | P3   | P4 | P5   | P6   | P7   | Latency | Recip. throughput\n---------------------------------------------------------------------------------------------------------------------\nPUSH %RBP                 | 1     | 0    | 0    | 0.33 | 0.33 | 1  | 0    | 0    | 0.33 | 3       | 1\nMOV %RSP,%RBP             | 1     | 0    | 0    | 0    | 0    | 0  | 0    | 0    | 0    | 0       | 0.25\nSUB $0x20,%RSP            | 1     | 0.25 | 0.25 | 0    | 0    | 0  | 0.25 | 0.25 | 0    | 1       | 0.25\nMOV %RDI,-0x18(%RBP)      | 1     | 0    | 0    | 0.33 | 0.33 | 1  | 0    | 0    | 0.33 | 3       | 1\nMOV %RSI,-0x20(%RBP)      | 1     | 0    | 0    | 0.33 | 0.33 | 1  | 0    | 0    | 0.33 | 3       | 1\nMOVL $0,-0x8(%RBP)        | 1     | 0    | 0    | 0.33 | 0.33 | 1  | 0    | 0    | 0.33 | 3       | 1\nJMP 84b <Chargement+0xc1> | 1     | 0    | 0    | 0    | 0    | 0  | 0    | 1    | 0    | 0       | 1-2\nNOP                       | 1     | 0    | 0    | 0    | 0    | 0  | 0    | 0    | 0    | 0       | 0.25\nLEAVE\nRET                       | 1     | 0    | 0    | 0.33 | 0.33 | 0  | 0    | 1    | 0.33 | 0       | 1\n",
+        },
+      },
+      header = {
+        "Warnings:\nThe number of fused uops of the instruction [LEAVE] is unknown",
+        "0% of peak computational performance is used (0.00 out of 4.00 FLOP per cycle (GFLOPS @ 1GHz))",
+      },
+      brief = {
+      },
+      gain = {
+        {
+          workaround = " - Try another compiler or update/tune your current one\n - Make array accesses unit-stride:\n  * If your function streams arrays of structures (AoS), try to use structures of arrays instead (SoA):\nfor(i) a[i].x = b[i].x; (slow, non stride 1) => for(i) a.x[i] = b.x[i]; (fast, stride 1)\n",
+          details = "All SSE/AVX instructions are used in scalar version (process only one data element in vector registers).\nSince your execution units are vector units, only a vectorized function can use their full power.\n",
+          title = "Vectorization",
+          txt = "Your function is not vectorized.\nOnly 20% of vector register length is used (average across all SSE/AVX instructions).\nBy vectorizing your function, you can lower the cost of an iteration from 4.00 to 0.75 cycles (5.33x speedup).",
+        },
+        {
+          workaround = " - Write less array elements\n - Provide more information to your compiler:\n  * use the 'restrict' C99 keyword\n",
+          title = "Execution units bottlenecks",
+          txt = "Performance is limited by writing data to caches/RAM (the store unit is a bottleneck).\n\nBy removing all these bottlenecks, you can lower the cost of an iteration from 4.00 to 2.25 cycles (1.78x speedup).\n",
+        },
+      },
+      potential = {
+      },
+    },
+  },
+  AVG = {
+      hint = {
+        {
+          title = "Type of elements and instruction set",
+          txt = "No instructions are processing arithmetic or math operations on FP elements. This function is probably writing/copying data or processing integer elements.",
+        },
+        {
+          title = "Matching between your function (in the source code) and the binary function",
+          txt = "The binary function does not contain any FP arithmetical operations.\nThe binary function is storing 20 bytes.",
+        },
+      },
+      expert = {
+        {
+          title = "General properties",
+          txt = "nb instructions    : 10\nnb uops            : 9\nloop length        : 31\nused x86 registers : 4\nused mmx registers : 0\nused xmm registers : 0\nused ymm registers : 0\nused zmm registers : 0\nnb stack references: 3\n",
+        },
+        {
+          title = "Front-end",
+          txt = "MACRO FUSION NOT POSSIBLE\nFIT IN UOP CACHE\nmicro-operation queue: 2.25 cycles\nfront end            : 2.25 cycles\n",
+        },
+        {
+          title = "Back-end",
+          txt = "       | P0   | P1   | P2   | P3   | P4   | P5   | P6   | P7\n--------------------------------------------------------------\nuops   | 0.50 | 0.25 | 1.67 | 1.67 | 4.00 | 0.25 | 2.00 | 1.67\ncycles | 0.50 | 0.25 | 1.67 | 1.67 | 4.00 | 0.25 | 2.00 | 1.67\n\nCycles executing div or sqrt instructions: NA\n",
+        },
+        {
+          title = "Cycles summary",
+          txt = "Front-end : 2.25\nDispatch  : 4.00\nOverall L1: 4.00\n",
+        },
+        {
+          title = "Vectorization ratios",
+          txt = "all     : 0%\nload    : NA (no load vectorizable/vectorized instructions)\nstore   : 0%\nmul     : NA (no mul vectorizable/vectorized instructions)\nadd-sub : NA (no add-sub vectorizable/vectorized instructions)\nfma     : NA (no fma vectorizable/vectorized instructions)\ndiv/sqrt: NA (no div/sqrt vectorizable/vectorized instructions)\nother   : NA (no other vectorizable/vectorized instructions)\n",
+        },
+        {
+          title = "Vector efficiency ratios",
+          txt = "all     : 20%\nload    : NA (no load vectorizable/vectorized instructions)\nstore   : 20%\nmul     : NA (no mul vectorizable/vectorized instructions)\nadd-sub : NA (no add-sub vectorizable/vectorized instructions)\nfma     : NA (no fma vectorizable/vectorized instructions)\ndiv/sqrt: NA (no div/sqrt vectorizable/vectorized instructions)\nother   : NA (no other vectorizable/vectorized instructions)\n",
+        },
+        {
+          title = "Cycles and memory resources usage",
+          txt = "Assuming all data fit into the L1 cache, each call to the function takes 4.00 cycles. At this rate:\n - 15% of peak store performance is reached (5.00 out of 32.00 bytes stored per cycle (GB/s @ 1GHz))\n",
+        },
+        {
+          title = "Front-end bottlenecks",
+          txt = "Found no such bottlenecks.",
+        },
+        {
+          title = "ASM code",
+          txt = "In the binary file, the address of the function is: 78a\n\nInstruction               | Nb FU | P0   | P1   | P2   | P3   | P4 | P5   | P6   | P7   | Latency | Recip. throughput\n---------------------------------------------------------------------------------------------------------------------\nPUSH %RBP                 | 1     | 0    | 0    | 0.33 | 0.33 | 1  | 0    | 0    | 0.33 | 3       | 1\nMOV %RSP,%RBP             | 1     | 0    | 0    | 0    | 0    | 0  | 0    | 0    | 0    | 0       | 0.25\nSUB $0x20,%RSP            | 1     | 0.25 | 0.25 | 0    | 0    | 0  | 0.25 | 0.25 | 0    | 1       | 0.25\nMOV %RDI,-0x18(%RBP)      | 1     | 0    | 0    | 0.33 | 0.33 | 1  | 0    | 0    | 0.33 | 3       | 1\nMOV %RSI,-0x20(%RBP)      | 1     | 0    | 0    | 0.33 | 0.33 | 1  | 0    | 0    | 0.33 | 3       | 1\nMOVL $0,-0x8(%RBP)        | 1     | 0    | 0    | 0.33 | 0.33 | 1  | 0    | 0    | 0.33 | 3       | 1\nJMP 84b <Chargement+0xc1> | 1     | 0    | 0    | 0    | 0    | 0  | 0    | 1    | 0    | 0       | 1-2\nNOP                       | 1     | 0    | 0    | 0    | 0    | 0  | 0    | 0    | 0    | 0       | 0.25\nLEAVE\nRET                       | 1     | 0    | 0    | 0.33 | 0.33 | 0  | 0    | 1    | 0.33 | 0       | 1\n",
+        },
+      },
+      header = {
+        "Warnings:\nThe number of fused uops of the instruction [LEAVE] is unknown",
+        "0% of peak computational performance is used (0.00 out of 4.00 FLOP per cycle (GFLOPS @ 1GHz))",
+      },
+      brief = {
+      },
+      gain = {
+        {
+          workaround = " - Try another compiler or update/tune your current one\n - Make array accesses unit-stride:\n  * If your function streams arrays of structures (AoS), try to use structures of arrays instead (SoA):\nfor(i) a[i].x = b[i].x; (slow, non stride 1) => for(i) a.x[i] = b.x[i]; (fast, stride 1)\n",
+          details = "All SSE/AVX instructions are used in scalar version (process only one data element in vector registers).\nSince your execution units are vector units, only a vectorized function can use their full power.\n",
+          title = "Vectorization",
+          txt = "Your function is not vectorized.\nOnly 20% of vector register length is used (average across all SSE/AVX instructions).\nBy vectorizing your function, you can lower the cost of an iteration from 4.00 to 0.75 cycles (5.33x speedup).",
+        },
+        {
+          workaround = " - Write less array elements\n - Provide more information to your compiler:\n  * use the 'restrict' C99 keyword\n",
+          title = "Execution units bottlenecks",
+          txt = "Performance is limited by writing data to caches/RAM (the store unit is a bottleneck).\n\nBy removing all these bottlenecks, you can lower the cost of an iteration from 4.00 to 2.25 cycles (1.78x speedup).\n",
+        },
+      },
+      potential = {
+      },
+    },
+  common = {
+    header = {
+      "The function is defined in /home/arezki/prog/multi.c:6-16.\n",
+      "Warnings:\nIgnoring paths for analysis",
+    },
+  },
+}
